278205535378b50f58d07dd834a9e9b9d9614a01
ThayanaRMachado <thayanarm@hotmail.com>
2020-08-26 19:24:05 -0300

    30-Criando Site Dinâmico-JavaScript/JQuery e PHP-Página contato

--- index.php
+++ index.php
@@ -12,6 +12,20 @@
 	<meta charset="utf-8" />
 </head>
 <body>
+
+	<?php
+		$url = isset($_GET['url']) ? $_GET['url'] : 'home';
+		switch ($url) {
+			case 'sobre':
+				echo '<target target="sobre" />';
+				break;
+
+			case 'servicos':
+				echo '<target target="servicos" />';
+				break;
+		}
+	?>
+
 	<header>
 		<div class="center">
 			<div class="logo left"><a href="/">Logomarca</a></div><!--logo-->
@@ -40,14 +54,17 @@
 	</header>
 
 	<?php
-		$url = isset($_GET['url']) ? $_GET['url'] : 'home';
 
 		if (file_exists('pages/'.$url.'.php')) {
 			include('pages/'.$url.'.php');
 		}else{
 			//Podemos fazer o que quiser, pois a página não existe.
-			$pagina404 = true;
-			include('pages/404.php');
+			if ($url != 'sobre' && $url != 'servicos') {
+				$pagina404 = true;
+				include('pages/404.php');
+			}else{
+				include('pages/home.php');
+			}
 		}
 	?>	
 
