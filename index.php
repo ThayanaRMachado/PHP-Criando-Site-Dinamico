e62c8e0a9fb519d9740dd8e383cfb9371cdb336d
ThayanaRMachado <thayanarm@hotmail.com>
2020-08-26 12:34:43 -0300

    1-Criando Site Dinâmico - HTML5 e Marcação 1de2

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto 01</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keywords" content="palavras-chave,do,meu,site">
+	<meta name="description" content="Descrição do meu website">
+	<meta charset="utf-8" />
+</head>
+<body>
+	<header>
+		<div class="center">
+			<div class="logo">Logomarca</div><!--logo-->
+			<nav class="desktop">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Depoimentos</a></li>
+					<li><a href="">Serviços</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav>
+			 <nav class="mobile">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Depoimentos</a></li>
+					<li><a href="">Serviços</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav>
+		</div><!--center-->
+	</header>
+
+	<section class="banner-principal">
+		<div class="center">
+			<form>
+				<h2>Qual o seu melho e-mail?</h2>
+				<input type="email" name="email" required>
+				<input type="submit" name="acao" value="Cadastrar">
+			</form>
+		</div><!--center-->
+	</section><!--banner-principal-->
+
+	<section class="descricao-autor">
+		<div class="center">
+			<div class="w50">
+				<h2>Guilherme C. Grillo</h2>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+			</div><!--w50-->
+			<div class="w50">
+				<!--Pegar imagem depois-->
+				<img src="images/foto.jpg">
+			</div><!--w50-->
+		</div><!--center-->
+	</section><!--descricao-autor-->
+
+	<section class="especialidades">
+
+		<div class="center">
+			<h2 class="title">Especialidades</h2>
+			<div class="box-especialidade">
+				<h3>icone1</h3>
+				<h3>CSS3</h3>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+			</div><!--box-especialidade-->
+			<div class="box-especialidade">
+				<h3>icone1</h3>
+				<h3>HTML5</h3>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+			</div><!--box-especialidade-->
+			<div class="box-especialidade">
+				<h3>icone1</h3>
+				<h3>JavaScript</h3>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+			</div><!--box-especialidade-->
+		</div><!--center-->
+	</section><!--especialidades-->
+
+	<section class="extras">
+		<div class="center">
+			<div class="w50">
+				<h2 class="title">Depoimentos</h2>
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+					<p class="nome-autor">Lorem Ipsum</p>
+				</div><!--depoimento-single-->
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+					<p class="nome-autor">Lorem Ipsum</p>
+				</div><!--depoimento-single-->
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+					<p class="nome-autor">Lorem Ipsum</p>
+				</div><!--depoimento-single-->
+			</div><!--w50-->
+			<div class="w50">
+				<h2 class="title">Serviços</h2>
+				<div class="servicos">
+					<ul>
+						<li>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia</li>
+						<li>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia</li>
+						<li>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia</li>
+					</ul>
+				</div><!--servicos-->
+			</div><!--w50-->
+		</div><!--center-->
+	</section><!--extras-->
+
+
+	<footer>
+		<div class="center">
+			<p>Todos os direitos reservados</p>
+		</div><!--center-->
+	</footer>
+</body>
+</html>
